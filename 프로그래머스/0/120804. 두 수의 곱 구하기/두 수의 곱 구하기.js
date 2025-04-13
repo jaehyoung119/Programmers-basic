06c99147ec9f04dd52a595931a64f5c5d666db2b
재형 <140633755+jaehyoung119@users.noreply.github.com>
2025-04-13 20:15:48 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.js
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.js
@@ -1,5 +1,5 @@
 function solution(num1, num2) {
-    const numRange = (n) => n >= -50000 && n <= 50000;
+    const numRange = (n) => n >= 0 && n <= 100;
     if (numRange(num1) && numRange(num2)) {
         return num1 * num2;
     }
