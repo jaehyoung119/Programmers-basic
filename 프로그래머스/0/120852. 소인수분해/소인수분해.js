1610466ae64400ff57b60a15d7d62e38d2b0c4f9
재형 <140633755+jaehyoung119@users.noreply.github.com>
2025-04-23 20:15:10 +0900

    [level 0] Title: 소인수분해, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  const result = [];
+
+  for (let i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      result.push(i);
+      n /= i;
+    }
+  }
+
+  return [...new Set(result)];
+}
