8249f0b1a8aa0c1b3a63f2c03fc4ec5902340fb5
재형 <140633755+jaehyoung119@users.noreply.github.com>
2025-05-30 18:03:09 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    let multi = 1;
+    let sum = 0;
+    
+    if(num_list.length >= 11) {
+        for(let i = 0; i < num_list.length; i++) {
+            sum += num_list[i]
+        }
+        
+        return sum;
+    } else {
+        for(let i = 0; i < num_list.length; i++) {
+            multi *= num_list[i]
+        }
+        
+        return multi;
+    }
+}
