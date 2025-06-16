2138fcd524565c97462d8f53957c5ab32c390c32
재형 <140633755+jaehyoung119@users.noreply.github.com>
2025-06-16 14:46:45 +0900

    [level 0] Title: 그림 확대, Time: 0.30 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -0,0 +1,13 @@
+function solution(picture, k) {
+    const result = [];
+
+    for (let row of picture) {
+        const expandedRow = [...row].map(char => char.repeat(k)).join("");
+
+        for (let i = 0; i < k; i++) {
+            result.push(expandedRow);
+        }
+    }
+
+    return result;
+}
