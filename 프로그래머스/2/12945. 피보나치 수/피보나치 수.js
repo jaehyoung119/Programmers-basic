5f808ac34e54dc7e0818f368ac06a39c3593eca7
재형 <140633755+jaehyoung119@users.noreply.github.com>
2025-08-01 15:08:45 +0900

    [level 2] Title: 피보나치 수, Time: 2.07 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    const MOD = 1234567;
+
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let prev = 0;
+    let curr = 1;
+
+    for (let i = 2; i <= n; i++) {
+        const next = (prev + curr) % MOD;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
+}
